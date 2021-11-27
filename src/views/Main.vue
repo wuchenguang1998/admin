388215a65f77952bd77a4bcda3b7b3d7478d0583
wuchenguang1998 <wuchenguang1998@sina.com>
2021-11-27 19:43:02 +0800

    主体

---
+++ src/views/Main.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-container style="height: 100%;">
+    <el-aside width="auto">
+      <CommonAside></CommonAside>
+    </el-aside>
+    <el-container>
+      <el-header>
+        <CommonHeader></CommonHeader>
+      </el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+  import CommonAside from '../components/commonAside'
+  import CommonHeader from '../components/commonHeader'
+export default {
+  name: "Main",
+  components: {
+    CommonAside,
+    CommonHeader
+  },
+};
+</script>
+
+<style scoped>
+.el-header {
+  background: #333;
+}
+.el-main {
+  padding-top: 0px;
+}
+</style>
